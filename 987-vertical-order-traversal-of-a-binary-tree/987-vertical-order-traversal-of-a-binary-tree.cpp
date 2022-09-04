a4d47c761a62f7984e944619b5f968d415a3a3b3
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-09-04 10:55:42 +0530

    Time: 11 ms (39.72%), Space: 13.1 MB (67.87%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,47 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-bool comparator(pair<int,int> x,pair<int,int> y){
-    if(x.second < y.second)
-        return true;
-    if(x.second > y.second)
-        return false;
-    
-    return x < y;
-    
-    
-    
-}
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         
-        map<int,vector<pair<int,int>>>up;
-        int row = 0;
-        int col = 0;
-        dfs(root,up,row,col);
-        vector<vector<int>> ans;
-        for(auto it = up.begin(); it!= up.end() ;it++){
-            //cout<<it->first<<" .... "<<it->second[0]<<endl;
-            sort(it->second.begin(),it->second.end(),comparator);
-            vector<int>tmp;
-            for(int i = 0 ; i < it->second.size();i++){
-                tmp.push_back(it->second[i].first);
-            }
-            ans.push_back(tmp);
-            
-        }
+         map<int,vector<pair<int,int>>>mp;
+         dfs(root,0,0,mp);
+         vector<vector<int>> ans;
+        
+         for(auto it = mp.begin();it!= mp.end() ;it++){
+               sort(it->second.begin(),it->second.end());
+             vector<int>curr;
+             for(int i = 0 ; i < it->second.size() ;i++){
+                 curr.push_back(it->second[i].second);
+             }
+             ans.push_back(curr);
+         }
         return ans;
         
     }
-    void dfs(TreeNode* root,map<int,vector<pair<int,int>>> &up,int row , int col){
+    void dfs(TreeNode* root , int row , int col , map<int,vector<pair<int,int>>>& mp){
         
         if(root == NULL)
-             return;
+            return;
         
-        up[col].push_back({root->val,row});
-        dfs(root->left,up,row+1,col-1);
-        dfs(root->right,up,row+1,col+1);
+        mp[col].push_back({row,root->val});
+        dfs(root->left,row+1,col-1,mp);
+        dfs(root->right,row+1,col+1,mp);
+        
         
         
     }
