9d1f4bfaa1bd59ca37f4059f897d9aedb83f312c
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-09-08 09:24:04 +0530

    Time: 3 ms (60.90%), Space: 8.5 MB (14.33%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -12,16 +12,43 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        dfs(root,ans);
-        return ans;
-    }
-    void dfs(TreeNode* root,vector<int>& ans){
+        
+        // iterative soln;
         if(root == NULL)
-            return ;
-        dfs(root->left,ans);
-        ans.push_back(root->val);
-        dfs(root->right,ans);
+            return {};
+        
+        stack<pair<TreeNode*,int>>st;
+        st.push({root,-1});
+        vector<int>ans;
+        
+        while(st.size() > 0){
+            int curr = st.top().second;
+            TreeNode* tmp = st.top().first;
+            
+            if(curr == -1 ){
+                st.top().second++;
+                
+            }else if(curr == 0){
+                st.top().second++;
+                if(tmp->left != NULL){
+                    st.push({tmp->left,-1});
+                   
+                }
+                 
+            }else if(curr == 1){
+                ans.push_back(tmp->val);
+                st.top().second++;
+                if(tmp->right != NULL)
+                    st.push({tmp->right,-1});
+                
+                
+            }else if(curr == 2){
+                st.pop();
+                
+            }
+            
+        }
+        return ans;
         
     }
 };
