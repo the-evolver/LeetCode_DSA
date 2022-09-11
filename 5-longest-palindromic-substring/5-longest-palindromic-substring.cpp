1a37e3bf34dd4bbf1ba68e0e4e81b40c316cfc81
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-09-11 12:07:21 +0530

    Time: 136 ms (56.55%), Space: 7.9 MB (69.13%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
+        int n = s.length();
+        bool dp[n][n];
         int max_len = 0;
         int st = -1 , en = -1;
         
-        // odd center 
-        for(int c = 0 ; c < s.length() ; c++){
-            for(int i = c , j = c ; i >= 0 && j < s.length();i--,j++){
-                if(s[i] != s[j])
-                    break;
-                if(j-i+1 > max_len){
-                    max_len = j - i + 1;
-                    st = i;
-                    en = j;
+        for(int g = 0 ; g < n ; g++){
+            for(int i = 0 , j = g ; j < n ; j++,i++){
+                 if(g == 0){
+                      dp[i][j] = true;
+                      if(max_len < 1){
+                          max_len = 1;
+                          st = i;
+                          en = j;
+                      }
+                 }
+                    
+                else if(g == 1){
+                    if(s[i] == s[j]){
+                         dp[i][j] = true;
+                        if(max_len < 2 ){
+                          max_len = 2;
+                          st = i;
+                          en = j;
+                      }
+                    }
+                       
+                    else 
+                        dp[i][j] = false;
                 }
-            }
-        }
-        
-        for(int c = 0 ; c < s.length() ; c++){
-            for(int i = c , j = c + 1 ; i >= 0 && j < s.length();i--,j++){
-                if(s[i] != s[j])
-                    break;
-                if(j-i+1 > max_len){
-                    max_len = j - i + 1;
-                    st = i;
-                    en = j;
+                else{
+                    if(s[i] == s[j] && dp[i+1][j-1] == true){
+                        dp[i][j] = true;
+                        if(max_len < j-i+1){
+                          max_len = j-i+1;
+                          st = i;
+                          en = j;
+                      }
+                    }
+                         
+                    else
+                        dp[i][j] = false;
+                    
                 }
             }
         }
         return s.substr(st,en-st+1);
-        
         
     }
 };
