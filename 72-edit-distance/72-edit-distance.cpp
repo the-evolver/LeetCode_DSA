6838773b8c0eeae78aece349172ef605be846618
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-09-14 23:41:03 +0530

    Time: 27 ms (41.44%), Space: 9.1 MB (24.06%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,8 +3,30 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return recur(n-1,m-1,word1,word2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // return recur(n-1,m-1,word1,word2,dp);
+        for(int i = 0 ; i <=n ; i++)dp[i][0] = i;
+        for(int j = 0 ; j <=m ; j++)dp[0][j] = j;
+        
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+               
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = min(1 + dp[i][j-1],
+                  min(1 + dp[i-1][j-1],1 + dp[i-1][j]));
+        
+
+                }
+        
+                
+                
+            }
+        }
+        
+        return dp[n][m];
     }
     
     int recur(int i , int j , string &s1 , string &s2,vector<vector<int>>&dp){
