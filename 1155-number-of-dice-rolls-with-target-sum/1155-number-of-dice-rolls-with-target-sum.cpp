dc3845cae78270e82c8f4f4f027705f6b228cda9
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-10-02 13:29:32 +0530

    Time: 97 ms (21.99%), Space: 8.4 MB (45.97%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,8 +2,29 @@ class Solution {
 public:
     int m = 1e9+7;
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<unsigned long long >>dp(n+1,vector<unsigned long long>(max(target,k)+1,-1));
-        return solve(n,k,target,dp);
+        
+       // vector<vector<unsigned long long >>dp(n+1,vector<unsigned long long>(max(target,k)+1,-1));
+        //return solve(n,k,target,dp);
+        
+        vector<vector<int>> dp(n+1,vector<int>(max(k,target) + 1 , 0));
+        dp[0][0] = 1 ; // dice bhi 0 and  target bhi 0 
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= target ; j++){
+                 //
+                int ans = 0;
+                for(int f = 1 ; f <= k ; f++){
+                    if(j - f >= 0)
+                    ans = (ans % m + dp[i-1][j-f] % m ) % m;
+                 }
+                 dp[i][j] = ans;
+                //
+            }
+        }
+        return dp[n][target];
+        
+        
+        
     }
     int solve(int dice , int face , int tar,vector<vector<unsigned long long>>& dp){
         
