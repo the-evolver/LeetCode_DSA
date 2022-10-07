da0cd29f2196040dfbb72d5af9dcfc843a0dcf25
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-10-07 12:39:44 +0530

    Time: 663 ms (73.98%), Space: 0B (100.00%) - LeetHub

---
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT  user_id,MAX(time_stamp ) AS last_stamp
+FROM LOGINS
+WHERE time_stamp > '2019-12-31 23:59:59' AND time_stamp < '2021-01-01 00:00:00' 
+GROUP BY user_id;
+
+
+
