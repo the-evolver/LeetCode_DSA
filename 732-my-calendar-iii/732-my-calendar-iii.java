46a8a641e602d5ec84360fd8cef7ea9f989c81c1
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-10-07 20:15:55 +0530

    Time: 234 ms (27.27%), Space: 54.9 MB (35.87%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.java
@@ -0,0 +1,17 @@
+class MyCalendarThree {
+    TreeMap<Integer, Integer> map;
+    public MyCalendarThree() {
+        map = new TreeMap<>();
+    }
+    
+    public int book(int start, int end) {
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) - 1);
+        int res = 0, count = 0;
+        for (int v : map.values()) {
+            count += v;
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+}
