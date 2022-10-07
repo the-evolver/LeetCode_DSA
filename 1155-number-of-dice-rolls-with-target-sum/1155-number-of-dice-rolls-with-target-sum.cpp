890c2635eec3402124e016e81a4bb624cc4e3105
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-10-07 19:24:10 +0530

    Time: 93 ms (26.46%), Space: 8.5 MB (25.44%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -3,47 +3,36 @@ public:
     int m = 1e9+7;
     int numRollsToTarget(int n, int k, int target) {
         
-       // vector<vector<unsigned long long >>dp(n+1,vector<unsigned long long>(max(target,k)+1,-1));
-        //return solve(n,k,target,dp);
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
         
-        vector<vector<int>> dp(n+1,vector<int>(max(k,target) + 1 , 0));
-        dp[0][0] = 1 ; // dice bhi 0 and  target bhi 0 
+         return helper(n,k,target,dp);
         
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= target ; j++){
-                 //
-                int ans = 0;
-                for(int f = 1 ; f <= k ; f++){
-                    if(j - f >= 0)
-                    ans = (ans % m + dp[i-1][j-f] % m ) % m;
-                 }
-                 dp[i][j] = ans;
-                //
-            }
-        }
-        return dp[n][target];
-        
-        
+       
         
     }
-    int solve(int dice , int face , int tar,vector<vector<unsigned long long>>& dp){
-        
-        if(tar < 0)
+    int helper(int dice , int faces , int target,vector<vector<int>>& dp){
+        if(target < 0 )
             return 0;
+        if(target == 0 && dice > 0)
+            return  dp[dice][target] = 0;
+        if(dice == 0 && target > 0)
+            return  dp[dice][target] = 0;
+        if(dice == 0 && target == 0)
+            return  dp[dice][target] = 1;
         
-        if(tar == 0 && dice != 0 ||  dice == 0 && tar != 0 )
-            return 0;
+        if(dp[dice][target] != -1)
+            return dp[dice][target];
         
-        if(tar == 0 && dice == 0)
-            return 1;
-        
-        if(dp[dice][tar] != -1 )
-            return dp[dice][tar];
-        
-        unsigned long long ans = 0;
-        for(int i = 1 ; i <= face ; i++){
-            ans = (ans % m + solve(dice-1,face,tar-i,dp) % m ) %m;
+        int sum = 0;
+        for(int i = 1 ; i <= faces ; i++){
+            if(dice - 1 >= 0 && target-i >= 0){
+            dp[dice-1][target-i] = helper(dice-1,faces,target-i,dp) % m ;
+            sum =  (sum % m + dp[dice-1][target-i] % m ) % m;
+          }
+            
         }
-        return dp[dice][tar] = ans;
+             //sum =  (sum % m + helper(dice-1,faces,target-i,dp) % m ) % m;
+        
+        return  dp[dice][target] = sum;
     }
 };
