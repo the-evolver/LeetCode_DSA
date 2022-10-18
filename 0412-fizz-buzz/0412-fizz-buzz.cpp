b4b6eb9513ca8728d65d0477355c61c797531f35
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-10-18 14:02:51 +0530

    Time: 11 ms (13.41%), Space: 7.9 MB (65.15%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         
-        vector<int> help(n+1,1);
-        for(int i = 3 ; i <= n ; i = i + 3){
-            help[i] *= 3;
-        }
-        for(int i = 5 ; i <= n ; i = i + 5){
-             help[i] *= 5;
-        }
+        int cnt3 = 0 , cnt5 = 0;
         
-        vector<string> ans(n);
-        for(int i = 1 ; i <= n ; i++){
-            if(help[i] == 15){
-                ans[i-1] = "FizzBuzz";
-            }
-            if(help[i] == 5){
-                ans[i-1] = "Buzz";
-            }
-            if(help[i] == 3){
-                ans[i-1] = "Fizz";
-            }
-            if(help[i] == 1){
-                ans[i-1] = to_string(i);
-            }
-        }
+         vector<string> ans;
+        
+         for(int i = 1 ; i <= n ; i++){
+                  cnt3++;
+                  cnt5++;
+                  string curr ="";
+             
+                 if(cnt3 == 3){
+                      curr += "Fizz";
+                      cnt3 = 0;
+                 }
+                 if(cnt5 == 5){
+                     curr += "Buzz";
+                     cnt5 = 0;
+                 }
+             
+             if(curr == ""){
+                 curr = to_string(i);
+             }
+                
+             ans.push_back(curr);
+             
+             
+         }
         return ans;
         
     }
