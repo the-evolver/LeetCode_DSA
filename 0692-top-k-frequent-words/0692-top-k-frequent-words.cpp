fd4b628c0f40a6d82920154ddedc010084d38b85
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-10-19 12:06:31 +0530

    Time: 20 ms (68.83%), Space: 12.5 MB (76.98%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,25 +1,34 @@
-bool comp(pair<string,int> p1, pair<string,int> p2 ){
-          
-        if(p1.second > p2.second){
+struct CompareHeight {
+    bool operator()(pair<string,int> p1, pair<string,int> p2 )
+    {
+         if(p1.second > p2.second){
             return true;
         }else if(p1.second < p2.second)
             return false;
         
             return p1.first < p2.first;
         
-        
     }
+};
+
+
+
+
 
 class Solution {
 public:
     
     
     vector<string> topKFrequent(vector<string>& words, int k) {
+        
         vector<string> ans;
         vector<pair<string,int>> s_ans;
         
         unordered_map<string,int> up;
+        priority_queue< pair<string,int> , vector<pair<string,int>> ,CompareHeight > pq;
+        
         int n = words.size();
+        
         for(int i = 0 ; i < n ; i++){
             up[words[i]]++;
         }
@@ -29,19 +38,19 @@ public:
               pair<string,int> curr;
               curr.first  = itr->first;
               curr.second = itr->second;
-              s_ans.push_back(curr);
+              pq.push(curr);
+              if(pq.size() > k)
+                  pq.pop();
         }
         
-        sort(s_ans.begin(),s_ans.end(),comp);
         
-        
-        for(int i = 1 , j = 0 ; i <= k ; i++){
-             
-                
-                 ans.push_back(s_ans[j++].first);
-                 
-             
+        while(pq.size() > 0){
+            ans.push_back(pq.top().first);
+            pq.pop();
         }
+        
+        for(int i = 0 , j = ans.size() - 1 ; i < j ; i++,j--)
+              swap(ans[i],ans[j]);
         
         return ans;
     }
