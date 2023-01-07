8bbd18ddd34ff406f901dfddd9d70d31fc3688e8
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-01-07 15:53:27 +0530

    Time: 12 ms (87.10%), Space: 10.3 MB (44.33%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,76 +2,63 @@ class Solution {
 public:
     void solve(vector<vector<char>>& board) {
         
-        int n = board.size();
-        int m = board[0].size();
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        // logic : agar koe bhi O boundary pe hai kisi region ka to region capture nahe hoga warna capture hoga.....
         
-        int i = 0 ,j = 0;
+        int row = board.size();
+        int col = board[0].size();
         
-        while( j < m ){
-            if(vis[i][j] == false && board[i][j] == 'O'){
-                dfs(board,vis,i,j,n,m);
+        vector<vector<bool>> vis(row,vector<bool>(col,false));
+        queue<pair<int,int>> qu;
+        
+        for(int i = 0 ; i < row ; i++){
+              for(int j = 0 ; j < col ; j++){
+                  
+                     if( board[i][j] =='O' && ( i == 0 || j == 0 || i == row-1 || j == col-1)){
+                         qu.push({i,j});
+                         vis[i][j] = true;
+                     }
+    
+    
+           }
+         }
+        
+        while(qu.size() > 0){
+            pair<int,int> co_or = qu.front();
+            qu.pop();
+            
+            int i = co_or.first;
+            int j = co_or.second;
+            
+            if(i-1 >= 0 && vis[i-1][j] == false && board[i-1][j] == 'O'){
+                vis[i-1][j] = true;
+                qu.push({i-1,j});
+
             }
-            j++;
+            if(j-1 >= 0 && vis[i][j-1] == false && board[i][j-1] == 'O'){
+                vis[i][j-1] = true;
+                qu.push({i,j-1});
+
+            }
+            if(i+1 < row && vis[i+1][j] == false && board[i+1][j] == 'O'){
+                    vis[i+1][j] = true;
+                    qu.push({i+1,j});
+            }
+            if(j+1 < col  && vis[i][j+1] == false && board[i][j+1] == 'O'){
+                        vis[i][j+1] = true;
+                        qu.push({i,j+1});
+
+            }
+            
         }
         
-        i = n-1;
-        j = 0;
-        
-        while( j < m ){
-            if(vis[i][j] == false && board[i][j] == 'O'){
-                dfs(board,vis,i,j,n,m);
-            }
-            j++;
-        }
-        
-        i = 0;
-        j = 0;
-        
-        while( i < n ){
-            if(vis[i][j] == false && board[i][j] == 'O'){
-                dfs(board,vis,i,j,n,m);
-            }
-            i++;
-        }
-        
-        j = m-1;
-        i = 0;
-         while( i < n ){
-            if(vis[i][j] == false && board[i][j] == 'O'){
-                dfs(board,vis,i,j,n,m);
-            }
-            i++;
-        }
-        
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                 cout<<vis[i][j]<<" ";
-                if(vis[i][j] == false && board[i][j] =='O')
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if(vis[i][j] == false){
                     board[i][j] ='X';
+                }
             }
-            cout<<endl;
         }
-        
-        
-        
-        return;
-        
-        
-    }
-    void dfs(vector<vector<char>>& board , vector<vector<bool>>& vis,int i , int j , int n , int m ){
-        
        
-        if(i < 0 || j < 0 || i == n || j == m || board[i][j] == 'X' || vis[i][j] == true)
-            return;
-        
-        vis[i][j] = true;
-        dfs(board,vis,i-1,j,n,m);
-        dfs(board,vis,i+1,j,n,m);
-        dfs(board,vis,i,j-1,n,m);
-        dfs(board,vis,i,j+1,n,m);
-        
-        
         
     }
 };
