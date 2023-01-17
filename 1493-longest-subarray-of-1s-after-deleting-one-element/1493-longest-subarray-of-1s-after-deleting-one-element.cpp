ed2b99f6087d70743ba2cd9546aedeaa9c21bfca
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-01-17 13:16:40 +0530

    Time: 48 ms (64.82%), Space: 36.5 MB (51.83%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -2,43 +2,31 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         
-        int n = nums.size();
-        int i = 0;
-        int ans = 0;
-        int cnt_0 = 0;
-        int cumm_sum = 0;
+        int n = nums.size(); // size of nums
+        int i = 0; // sliding window starting point
+        int ans = 0;  // ans variable stores ans
+        int cnt_0 = 0; // keeps track of no of zeros in current window
+        int cumm_sum = 0; // ** cumm_sum of sliding window
+        
         for(int j = 0 ; j < n ; j++){
             
             if(nums[j] == 1)
-              cumm_sum++;
+              cumm_sum++; // adding in window
             else{
                 cnt_0++;
             }
-            
             while(cnt_0 > 1){
-                  cumm_sum = cumm_sum - nums[i];
+                  cumm_sum = cumm_sum - nums[i]; // removing from window
                   if(nums[i] == 0){
                       cnt_0--;
                   }
                  i++;
-                
-                
-                
             }
-            
-            
-            
-            
             ans = max(ans,cumm_sum);
-            
-            
-            
         }
         if( ans == n)
             ans--;
         
         return ans;
-        
-        
     }
 };
