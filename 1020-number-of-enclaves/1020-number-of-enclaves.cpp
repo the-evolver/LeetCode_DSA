2666c9d09ab5bcad4684e9fc92e4d7982e1f5164
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-01-24 18:53:25 +0530

    Time: 176 ms (20.78%), Space: 31.5 MB (40.20%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -3,65 +3,56 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         
         
-        int row  = grid.size();
-        int col = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
         
-        vector<vector<bool>>vis(row,vector<bool>(col,false));
-        queue<pair<int,int>> qu;
-        for(int i = 0 ; i  < row ; i++){
-            for(int j = 0 ; j < col ; j++){
-                 
-                if( grid[i][j] == 1 && ( i == 0 || i + 1 == row || j == 0 || j+1 == col ) ){
-                    qu.push({i,j});
-                    vis[i][j] = true;
-                    
-                    
-                }
-            
+        
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        for(int i = 0 ; i < n ; i++){
+             if(grid[i][0] == 1 ){
+                 dfs(grid,vis,i,0,n,m);
+             }
+             if(grid[i][m-1] == 1){
+                 dfs(grid,vis,i,m-1,n,m);
+             }
+        }
+        
+        for(int j = 0 ; j < m ; j++){
+             if(grid[0][j] == 1 ){
+                 dfs(grid,vis,0,j,n,m);
+             }
+             if(grid[n-1][j] == 1){
+                 dfs(grid,vis,n-1,j,n,m);
+             }
+        }
+        
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 1 && vis[i][j] == false)
+                    ans++;
             }
         }
         
-        while(qu.size() > 0){
-            
-            pair<int,int> co = qu.front();
-            qu.pop();
-            int i = co.first;
-            int j = co.second;
-            
-            if(i-1 >= 0 && vis[i-1][j] == false && grid[i-1][j] == 1){
-                vis[i-1][j] = true;
-                qu.push({i-1,j});
-            }
-            if(j-1 >= 0 && vis[i][j-1] == false && grid[i][j-1] == 1){
-                vis[i][j-1] = true;
-                qu.push({i,j-1});
-            }
-            if(i+1 < row && vis[i+1][j] == false && grid[i+1][j] == 1){
-                vis[i+1][j] = true;
-                qu.push({i+1,j});
-            }
-            if(j+1 < col && vis[i][j+1] == false && grid[i][j+1] == 1){
-                vis[i][j+1] = true;
-                qu.push({i,j+1});
-            }
-            
-            
-            
-        }
+        return ans;
         
-        int cnt = 0;
+    }
+    
+    void dfs(vector<vector<int>>& grid , vector<vector<bool>>& vis , int i , int j ,int n , int m ){
         
-        for(int i = 0 ; i < row ; i++){
-            for(int j = 0 ; j < col ; j++){
-                
-                if(grid[i][j] == 1 && vis[i][j] == false){
-                    cnt++;
-                }
-                
-            }
-        }
+        if(i < 0 || i >= n || j < 0 || j >= m || vis[i][j] == true || grid[i][j] == 0)
+            return;
         
-        return cnt;
+         vis[i][j] = true;
+        
+        dfs(grid,vis,i-1,j,n,m);
+        dfs(grid,vis,i+1,j,n,m);
+        dfs(grid,vis,i,j-1,n,m);
+        dfs(grid,vis,i,j+1,n,m);
+        
+        
+        
+        
         
     }
 };
