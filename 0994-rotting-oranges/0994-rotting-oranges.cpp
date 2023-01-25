3a593807a31e1e2d85f83d370b1406c8dfb5c30b
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-01-25 15:05:55 +0530

    Time: 3 ms (97.11%), Space: 13.5 MB (17.23%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,79 +2,65 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         
-        int row = grid.size();
-        int col = grid[0].size();
-        
-         vector<vector<bool>>vis(row,vector<bool>(col,false));
-         vector<vector<int>>ans(row,vector<int>(col,0));
+        // logic --> bfs ( why ? : because questiona says 4-directionally adjacent)
+        int n = grid.size();
+        int m = grid[0].size();
         
         queue<pair<int,int>> qu;
-        for(int i = 0 ; i < row ; i++){
-            for(int j = 0 ; j < col ; j++){
-                if(grid[i][j] == 2){
+        vector<vector<int>> ans(n,vector<int>(m,0));
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 2 ){
                     qu.push({i,j});
                     vis[i][j] = true;
-                
+                    
                 }
-                
             }
         }
         
-        while(qu.size() > 0 ){
-            
-            pair<int,int> pr = qu.front();
+        while(qu.size() > 0){
+            auto it = qu.front();
             qu.pop();
             
-            int i = pr.first;
-            int j = pr.second;
+            int i = it.first ;
+            int j = it.second;
             
-            if(i-1 >= 0 && vis[i-1][j] == false && grid[i-1][j] == 1){
+            if(i-1 >= 0 && grid[i-1][j] != 0 && vis[i-1][j] == false){
+                vis[i-1][j] = true;
                 qu.push({i-1,j});
                 ans[i-1][j] = ans[i][j] + 1;
-                vis[i-1][j] = true;
-                grid[i-1][j] = 2;
-                
             }
-            if(j-1 >= 0 && vis[i][j-1] == false && grid[i][j-1] == 1){
+            if(j-1 >= 0 && grid[i][j-1] != 0 && vis[i][j-1] == false){
+                vis[i][j-1] = true;
                 qu.push({i,j-1});
                 ans[i][j-1] = ans[i][j] + 1;
-                vis[i][j-1] = true;
-                grid[i][j-1] = 2;
-                
             }
-            if(i+1 < row && vis[i+1][j] == false && grid[i+1][j] == 1){
+            if(i+1 < n && grid[i+1][j] != 0 && vis[i+1][j] == false){
+                vis[i+1][j] = true;
                 qu.push({i+1,j});
                 ans[i+1][j] = ans[i][j] + 1;
-                vis[i+1][j] = true;
-                grid[i+1][j] = 2;
-                
             }
-            if(j+1 < col && vis[i][j+1] == false && grid[i][j+1] == 1){
+            if(j+1 < m && grid[i][j+1] != 0 && vis[i][j+1] == false){
+                vis[i][j+1] = true;
                 qu.push({i,j+1});
                 ans[i][j+1] = ans[i][j] + 1;
-                vis[i][j+1] = true;
-                grid[i][j+1] = 2;
-                
             }
             
             
             
         }
+        
         int cnt = 0;
-        for(int i = 0 ; i < row ; i++){
-            for(int j = 0 ; j < col ; j++){
-                 cnt = max(cnt,ans[i][j]);
-                 if(grid[i][j] == 1){
-                     //cout<<i<<"  "<<j<<endl;
-                     return -1;
-                 }
-                     
-            
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 1 && vis[i][j] == false)
+                    return -1;
+                cnt = max(cnt,ans[i][j]);
             }
         }
-       return cnt;
-        
-        
+        return cnt;
         
     }
 };
