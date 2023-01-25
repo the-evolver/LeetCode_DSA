61bcd3adf7b01bd05e029e3a7e1738612a448712
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-01-25 14:34:25 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -9,77 +9,57 @@ class Solution
     //Function to find distance of nearest 1 in the grid for each cell.
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
-	     
-	     
-	     /*
-	     
-	     logic : apun ko nearest 1 nikalna hai mtlb
-	             kisi bhi cell se nearest 1 kahan hai horizontal and vertical 
-	             movement allowed hai.... 
-	     kya soch skte hain ----> bfs level wise kyun ki adjacent me kaam ho raha
-	     
-	     */
-	     
-	     int row = grid.size();
-	     int col = grid[0].size();
-	     
-	     vector<vector<int>>  ans(row,vector<int>(col,0));
-	     vector<vector<bool>> vis(row,vector<bool>(col,false));
-	     
-	     queue<pair<int,int>>qu;
-	     
-	     for(int i = 0 ; i < row ; i++){
-	         for(int j = 0 ; j < col ; j++){
-	             if(grid[i][j] == 1){
-	                 
-	                  qu.push({i,j});
-	                  vis[i][j] = true;
-	                  
-	             }
-	         }
-	     }
-	     
-	     
-	     while(qu.size() > 0){
-	         
-	         pair<int,int> co_or = qu.front();
-	         qu.pop();
-	         
-	         int i = co_or.first;
-	         int j = co_or.second;
-	         
-	         if(i-1>= 0 && vis[i-1][j] == false){
-	             ans[i-1][j] = ans[i][j] + 1;
-	             vis[i-1][j] = true;
-	             qu.push({i-1,j});
-	             
-	         }
-	         if(j-1>= 0 && vis[i][j-1] == false){
-	             ans[i][j-1] = ans[i][j] + 1;
-	             vis[i][j-1] = true;
-	             qu.push({i,j-1});
-	             
-	         }
-	         if(i+1 < row  && vis[i+1][j] == false){
-	             ans[i+1][j] = ans[i][j] + 1;
-	             vis[i+1][j] = true;
-	             qu.push({i+1,j});
-	             
-	         }
-	         if(j+1 < col && vis[i][j+1] == false){
-	             ans[i][j+1] = ans[i][j] + 1;
-	             vis[i][j+1] = true;
-	             qu.push({i,j+1});
-	             
-	         }
-	         
-	         
-	     }
-	     
-	     
-	     return ans;
-	     
-	     
+	    int n = grid.size();
+	    int m = grid[0].size();
+	    vector<vector<int>> ans(n,vector<int>(m,0));
+	    
+	    queue<pair<int,int>> qu;
+	    vector<vector<bool>> vis(n,vector<bool>(m,false));
+	    
+	    for(int i = 0 ; i < n ; i++){
+	        for(int j = 0 ; j < m ; j++){
+	            if(grid[i][j] == 1){
+	                vis[i][j] = true;
+	                qu.push({i,j});
+	            }
+	        }
+	    }
+	    
+	    while(qu.size() > 0 ){
+	        
+	        auto it = qu.front();
+	        qu.pop();
+	        
+	        int i = it.first;
+	        int j = it.second;
+	        
+	        if(i-1 >= 0 && vis[i-1][j] == false){
+	            ans[i-1][j] = ans[i][j] + 1;
+	            qu.push({i-1,j});
+	            vis[i-1][j] = true;
+	        }
+	        if(j-1 >= 0 && vis[i][j-1] == false){
+	            ans[i][j-1] = ans[i][j] + 1;
+	            qu.push({i,j-1});
+	            vis[i][j-1] = true;
+	        }
+	        if(i+1 < n && vis[i+1][j] == false){
+	            ans[i+1][j] = ans[i][j] + 1;
+	            qu.push({i+1,j});
+	            vis[i+1][j] = true;
+	        }
+	        if(j+1 < m && vis[i][j+1] == false){
+	            ans[i][j+1] = ans[i][j] + 1;
+	            qu.push({i,j+1});
+	            vis[i][j+1] = true;
+	        }
+	        
+	    }
+	    
+	    return ans;
+	    
+	    
+	    
 	}
 };
 
