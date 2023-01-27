bc2d7be65d439a65b7db4c231c2b74b77eb81513
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-01-27 15:11:11 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -10,49 +10,47 @@ using namespace std;
 class Solution {
   public:
     int countDistinctIslands(vector<vector<int>>& grid) {
+         
+         int n = grid.size();
+         int m = grid[0].size();
+         set<vector<pair<int,int>>> ans;
+         
+         vector<vector<bool>> vis(n,vector<bool>(m,false));
+         
+         for(int i = 0 ; i < n ; i++){
+             for(int j = 0 ; j < m ; j++){
+                 
+                 if(vis[i][j] == false && grid[i][j] == 1){
+                     vector<pair<int,int>> v;
+                     dfs(grid,vis,i,j,n,m,i,j,v);
+                     ans.insert(v);
+                 }
+                 
+             }
+         }
+         
+         
+         return ans.size();
+         
+         
+    }
+    
+    
+    void dfs(vector<vector<int>>& grid , vector<vector<bool>>& vis ,int i , int j , int row , int col ,int base_i,int base_j,vector<pair<int,int>>& v){
         
-        
-        set< vector<pair<int,int>> > st;
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == 1 && vis[i][j] == false ){
-                    vector<pair<int,int>> v ;
-                    dfs(grid,vis,i,j,n,m,v,i,j);
-                    st.insert(v);
-                }
-            }
-        }
-        
-        return st.size();
-        
-        
-        
+         if( i < 0 || i == row || j < 0 || j == col || grid[i][j] == 0 || vis[i][j] == true)
+              return;
+              
+              vis[i][j] = true;
+              v.push_back({i-base_i,j-base_j});
+              dfs(grid,vis,i-1,j,row,col,base_i,base_j,v);
+               dfs(grid,vis,i+1,j,row,col,base_i,base_j,v);
+                dfs(grid,vis,i,j-1,row,col,base_i,base_j,v);
+                 dfs(grid,vis,i,j+1,row,col,base_i,base_j,v);
+             
         
     }
-    void dfs(vector<vector<int>>& grid ,vector<vector<bool>>& vis ,int i , int j , int row , int col , vector<pair<int,int>>& v , int base_i , int base_j){
-        
-        if(i < 0 || j < 0 || i >= row || j >= col || grid[i][j] != 1 || vis[i][j] == true)
-             return;
-             
-             
-        v.push_back({i-base_i,j-base_j});
-        vis[i][j] = true;
-        
-           dfs(grid,vis,i-1,j,row,col,v,base_i,base_j);
-           dfs(grid,vis,i+1,j,row,col,v,base_i,base_j);
-           dfs(grid,vis,i,j-1,row,col,v,base_i,base_j);
-           dfs(grid,vis,i,j+1,row,col,v,base_i,base_j);
-        
-        
-        
-        
-    }
+    
 };
 
 
