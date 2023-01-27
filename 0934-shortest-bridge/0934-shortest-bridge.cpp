aaff0eec748b2368b62ea6702220c48f7546f402
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-01-27 19:19:31 +0530

    Time: 58 ms (68.80%), Space: 20.7 MB (56.65%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -5,62 +5,105 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        
-        vector<pair<int,int>> island1;
-        
-        vector<pair<int,int>> island2;
-        
+        queue<pair<int,int>> qu;
         int cnt = 0;
-        
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
         for(int i = 0 ; i < n ; i++){
-            
-                for(int j = 0 ; j < m ; j++){
-                    
-                         if(vis[i][j] == false && grid[i][j] == 1){
-                             
-                                 if(cnt == 0){
-                                     dfs(grid,i,j,n,m,vis,island1);
-                                     cnt++;
-                                 }else{
-
-                                     dfs(grid,i,j,n,m,vis,island2);
-
-                                 }
-                        }
-                    
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 1){
+                    dfs(grid,i,j,n,m,qu,vis);
+                    cnt++;
                 }
-        }
-        
-        int minm = INT_MAX;
-        for(int i = 0 ; i < island1.size() ; i++){
-            for(int j = 0 ; j < island2.size();j++){
-                minm = min(minm,abs(island1[i].first-island2[j].first) + abs(island1[i].second-island2[j].second));
+                if(cnt > 0)
+                    break;
             }
+            if(cnt > 0)
+                    break;
+        
         }
-        
-        return minm-1;
-        
-        
-       
+            
+       int ans = 0;
+    
+       qu.push({-1,-1});
+            
+       while(qu.size() > 0){
+           
+           auto it = qu.front();
+           qu.pop();
+           
+           int i = it.first;
+           int j = it.second;
+           //cout<<i<<" "<<j<<endl;
+           if(i == -1){
+               ans++;
+               qu.push({-1,-1});
+           }
+           else{
+               
+               if(i - 1 >= 0 && vis[i-1][j] == false){
+                   
+                   if(grid[i-1][j] == 1){
+                        return ans;
+                   }
+                      
+                   vis[i-1][j] = true;
+                   qu.push({i-1,j});
+                   
+               }
+                if(j - 1 >= 0 && vis[i][j-1] == false){
+                   if(grid[i][j-1] == 1){
+                        return ans;
+                   }
+                      
+                   vis[i][j-1] = true;
+                   qu.push({i,j-1});
+                   
+               }
+               if(i + 1 < n && vis[i+1][j] == false){
+                   if(grid[i+1][j] == 1){
+                        return ans;
+                   }
+                      
+                   vis[i+1][j] = true;
+                   qu.push({i+1,j});
+               }
+               if(j+1 < m && vis[i][j+1] == false){
+                   if(grid[i][j+1] == 1){
+                        return ans;
+                   }
+                      
+                   vis[i][j+1] = true;
+                   qu.push({i,j+1});
+               }
+               
+               
+               
+           }
+               
+       }
+            
+            
+            
+        return 0;
     }
-    
-    void dfs(vector<vector<int>>& grid, int i , int j , int n , int m , vector<vector<bool>>& vis , vector<pair<int,int>>& ans){
-        
-        if( i < 0 || j < 0 || i == n || j == m || grid[i][j] == 0 || vis[i][j] == true)
-            return;
-        
-        vis[i][j] = true;
-        
-        ans.push_back({i,j});
-        
-        dfs(grid,i-1,j,n,m,vis,ans);
-        dfs(grid,i+1,j,n,m,vis,ans);
-        dfs(grid,i,j-1,n,m,vis,ans);
-        dfs(grid,i,j+1,n,m,vis,ans);
-        
-        
-    }
-    
-    
+     void dfs(vector<vector<int>>& grid , int i , int j , int n , int m , queue<pair<int,int>>& qu , vector<vector<bool>>& vis){
+         
+         if(i < 0 || j < 0 || i == n || j == m || vis[i][j] == true || grid[i][j] == 0){
+             return ;
+         }
+         
+         vis[i][j] = true;
+         qu.push({i,j});
+         
+         dfs(grid,i-1,j,n,m,qu,vis);
+         dfs(grid,i+1,j,n,m,qu,vis);
+         dfs(grid,i,j-1,n,m,qu,vis);
+         dfs(grid,i,j+1,n,m,qu,vis);
+         
+         
+         
+         
+         
+         
+     }
 };
