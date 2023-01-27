5eeb7c1c0b08466c470aa7389bab829efe281ec9
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-01-27 15:34:33 +0530

    Time: 131 ms (36.59%), Space: 21.8 MB (40.63%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -2,85 +2,66 @@ class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
         
+        
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-         
-        vector<vector<int>> ans(n,vector<int>(m,0));
         queue<pair<int,int>> qu;
+        
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        
+        vector<vector<int>>  ans(n,vector<int>(m,0));
         
         
         for(int i = 0 ; i < n ; i++){
+            
             for(int j = 0 ; j < m ; j++){
+                
                 if(grid[i][j] == 1){
                     vis[i][j] = true;
                     qu.push({i,j});
                 }
+                
             }
         }
-        
-        int max_min = -1;
-        
+        int maxi = 0;
         while(qu.size() > 0 ){
             
-            auto it = qu.front();
-            qu.pop();
+               auto it = qu.front();
+               qu.pop();
             
-            int i = it.first;
-            int j = it.second;
-            
-            if(i - 1 >= 0 && vis[i-1][j] == false){
-                
-                ans[i-1][j] = ans[i][j] + 1;
-                vis[i-1][j] = true;
-                qu.push({i-1,j});
-                max_min = max(max_min,ans[i-1][j]);
-                
-            }
-            
-             
-            if(j - 1 >= 0 && vis[i][j-1] == false){
-                
-                ans[i][j-1] = ans[i][j] + 1;
-                vis[i][j-1] = true;
-                qu.push({i,j-1});
-                max_min = max(max_min,ans[i][j-1]);
-                
-            }
-            
-             
-            if(i + 1 < n && vis[i+1][j] == false){
-                ans[i+1][j] = ans[i][j] + 1;
-                vis[i+1][j] = true;
-                qu.push({i+1,j});
-                max_min = max(max_min,ans[i+1][j]);
-            }
-            
-             
-            if(j + 1 < m && vis[i][j+1] == false){
-                ans[i][j+1] = ans[i][j] + 1;
-                vis[i][j+1] = true;
-                qu.push({i,j+1});
-                max_min = max(max_min,ans[i][j+1]);
-            }
-            
-            
-            
-            
-            
-            
-            
+              int i = it.first;
+              int j = it.second;
+               
+              if(i - 1 >= 0 && vis[i-1][j] == false ){
+                  ans[i-1][j] = ans[i][j] + 1;
+                  vis[i-1][j] = true;
+                  maxi = max(maxi,ans[i-1][j]);
+                  qu.push({i-1,j});
+              }
+             if(i + 1 < n  && vis[i+1][j] == false ){
+                  ans[i+1][j] = ans[i][j] + 1;
+                  vis[i+1][j] = true;
+                  qu.push({i+1,j});
+                 maxi = max(maxi,ans[i+1][j]);
+              }
+             if(j - 1 >= 0 && vis[i][j-1] == false ){
+                  ans[i][j-1] = ans[i][j] + 1;
+                  vis[i][j-1] = true;
+                  qu.push({i,j-1});
+                 maxi = max(maxi,ans[i][j-1]);
+              }
+             if(j + 1 < m  && vis[i][j+1] == false ){
+                  ans[i][j+1] = ans[i][j] + 1;
+                  vis[i][j+1] = true;
+                  qu.push({i,j+1});
+                 maxi = max(maxi,ans[i][j+1]);
+              }
+           
         }
         
         
-        
-        
-        
-        return max_min;
-        
-        
-        
+        return maxi == 0 ? -1 : maxi;
         
     }
 };
