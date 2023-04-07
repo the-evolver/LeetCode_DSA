a24b5a809b50675a5b74eb7dfc9a477c88bb17a1
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-04-07 15:37:19 +0530

    Time: 180 ms (72.41%), Space: 77.4 MB (50.21%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,19 +2,31 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         
-        unordered_map<int,int>up;
-        int len = nums.size();
-        for(int i = 0 ; i < len ; i++){
-             if(up.find(nums[i]) == up.end())
-                 up[nums[i]] = i;
-             else{
-                 if(i - up[nums[i]] <= k)
-                     return true;
-                 up[nums[i]] = i;
-                 
-             }
+        // sliding window --> max size k if any sliding window contains the distinct valuse then ans is true...
+        // for each window we have to calc the value..
+        // bruteforce -> using hashmap if the cnt becomes twice and j-i <= k return true;
+        
+        unordered_map<int,int> up;
+        int len = 0;
+        int n = nums.size();
+        
+        for(int j = 0 ; j < n ; j++){
+            
+            if(len >= k + 1 ){
+                up[nums[j-1-len + 1]]--;
+                len--;
+                
+            }
+            
+            up[nums[j]]++;
+            len++;
+            
+            if(up[nums[j]] >= 2)
+                return true;
+            
             
         }
+        
         return false;
     }
 };
