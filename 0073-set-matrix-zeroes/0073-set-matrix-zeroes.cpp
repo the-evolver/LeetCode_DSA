bc22470f3a7071b55f02bb94b7f2d5d5e4672815
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-07-03 00:37:14 +0530

    Time: 25 ms (9.28%), Space: 13.6 MB (8.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
         set<int> rows;
         set<int> cols;
         
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
+        for(int i = 0 ; i < matrix.size() ; i++){
+            for(int j = 0 ; j < matrix[i].size();j++){
+                if(matrix[i][j] == 0){
+                    rows.insert(i);
+                    cols.insert(j);
+                }
 
-             if(matrix[i][j] == 0){
-                 rows.insert(i);
-                 cols.insert(j);
-               }
-            
             }
         }
         
-        for(auto itr = rows.begin(); itr != rows.end() ; itr++ ){
+        for( auto it = rows.begin() ; it!= rows.end() ; it++ ){
             
-             int row = (*itr) ;
-             for(int j = 0 ; j < m ; j++){
-                   matrix[row][j] = 0;
-             
-             }
-        
-        }
-        
-        for(auto itr = cols.begin(); itr != cols.end() ; itr++ ){
+              int curr_row = *it;
+              for(int j = 0 ; j < matrix[0].size() ; j++){
+                   matrix[curr_row][j] = 0;
+           }
             
-             int col = (*itr) ;
-             for(int j = 0 ; j < n ; j++){
-                   matrix[j][col] = 0;
-             
-             }
+         }
+        for( auto it = cols.begin() ; it!= cols.end() ; it++ ){
+            
+              int curr_col = *it;
+            
+              for(int i = 0 ; i < matrix.size() ; i++){
+                   matrix[i][curr_col] = 0;
+           }
+            
+         }
+       
         
-        }
         
         return ;
         
