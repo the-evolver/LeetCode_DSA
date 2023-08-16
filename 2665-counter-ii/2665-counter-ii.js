d503fd0fd0adadc644eb1188b8b9212d9490efda
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2023-08-16 16:30:14 +0530

    Time: 54 ms (91.34%), Space: 43.9 MB (97.47%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,38 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+
+var createCounter = function(init) {
+    let cnt = true;
+    let j;
+    
+    if(cnt === true){
+         j = init;
+        cnt = false;
+    }
+       
+    
+    
+    return {
+        increment:function(){
+            return ++init;
+        },
+        decrement:function(){
+            return --init;
+        },
+        reset:function(){
+            init = j;
+            return init;
+        },
+ 
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
